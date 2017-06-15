defbf489ee65fc937e6988f253804f7c59944675
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-06-15 11:19:06 +0700

    Computed property, easy

--- app/Model/Reservation.js
+++ app/Model/Reservation.js
@@ -3,8 +3,16 @@
 const Lucid = use('Lucid')
 
 class Reservation extends Lucid {
+  static get computed () {
+    return ['xyz'];
+  }
+
 	static get table () {
     return 'res_reservation'
+  }
+
+  getXyz(){
+    return 'xyz';
   }
 }
 
